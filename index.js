62b615c54578ceb387a287424f15a5e978fbc056
Fer Hernandez <fh.1111@gmail.com>
2019-02-05 02:15:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -42,3 +42,7 @@ function generateCartItem(itemName) {
     itemPrice:getRandomInt(1, 100)
   }
 }
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
